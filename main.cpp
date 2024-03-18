14a85845d564005c269900dc148a6514400fdabe
Vinícius Ayres <vini.na.ayres@gmail.com>
2024-03-18 09:28:54 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,50 +1,48 @@
 #include <iostream>
-#include <vector>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
-void embaralhar(vector<string>& baralho) {
-  srand(time(0));
-  int n = baralho.size();
-  for (int i = 0; i < n; ++i) {
-    int r = rand();
-    int j = r % n;
-    swap(baralho[i], baralho[j]);
-  }
+void embaralhar(string* baralho, int tamanho) {
+    srand(time(0));
+    for (int i = 0; i < tamanho; ++i) {
+        int r = rand() % tamanho;
+        swap(baralho[i], baralho[r]);
+    }
 }
 
 int main() {
-  vector<string> baralho(104);
+    const int tamanho_baralho = 104;
+    string baralho[tamanho_baralho];
 
-  for (int n = 0; n < 4; ++n) {
-    for (int i = 1; i <= 13; ++i) {
-      for (int b = 1; b <= 2; ++b) {
-        string s = to_string(n + 1) + "-" + (i < 10 ? "0" : "") + to_string(i) + "-" + to_string(b);
-        baralho[(n * 26) + ((i - 1) * 2) + (b - 1)] = s;
-      }
+    for (int n = 0; n < 4; ++n) {
+        for (int i = 1; i <= 13; ++i) {
+            for (int b = 1; b <= 2; ++b) {
+                string s = to_string(n + 1) + "-" + (i < 10 ? "0" : "") + to_string(i) + "-" + to_string(b);
+                baralho[(n * 26) + ((i - 1) * 2) + (b - 1)] = s;
+            }
+        }
     }
-  }
 
-  embaralhar(baralho);
+    embaralhar(baralho, tamanho_baralho);
 
-  vector<vector<string>> mao(4, vector<string>(11));
-  int cartaAtual = 0;
+    string mao[4][11];
+    int cartaAtual = 0;
 
-  for (int j = 0; j < 4; ++j) {
-    for (int c = 0; c < 11; ++c) {
-      mao[j][c] = baralho[cartaAtual++];
+    for (int j = 0; j < 4; ++j) {
+        for (int c = 0; c < 11; ++c) {
+            mao[j][c] = baralho[cartaAtual++];
+        }
     }
-  }
 
-  for (int j = 0; j < 4; ++j) {
-    cout << "Mão do Jogador " << j + 1 << ":\n";
-    for (int c = 0; c < 11; ++c) {
-      cout << mao[j][c] << endl;
+    for (int j = 0; j < 4; ++j) {
+        cout << "Mão do Jogador " << j + 1 << ":\n";
+        for (int c = 0; c < 11; ++c) {
+            cout << mao[j][c] << endl;
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 
-  return 0;
+    return 0;
 }
